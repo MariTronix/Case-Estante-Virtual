fbf70e19f47b19b73a31359bc5da76ef4d2ca525
Mariana Mendes <filhatronixmmlima@gmail.com>
2022-08-05 10:31:10 -0300

    Type: CompetitionDTO, ResultDTO, UpdateStatusDTO

---
+++ src/types/DTO/CompetitionDTO.ts
@@ -0,0 +1,11 @@
+export interface CompetitionInputDTO {
+    name: string,
+    date: string
+};
+
+export interface CompetitionInputMockDTO {
+    id: string,
+    name: string,
+    date: string,
+    status: string
+}
